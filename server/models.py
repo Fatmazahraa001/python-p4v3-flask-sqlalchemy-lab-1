50ddbd8e5aba4adfec179926af200e6135915ab6
Fatuma Ibrahim <fatmazahraabdulrashid@gmail.com>
2024-10-13 20:09:26 +0300

    complete

--- server/models.py
+++ server/models.py
@@ -6,13 +6,13 @@ from sqlalchemy_serializer import SerializerMixin
 db = SQLAlchemy()
 
 class Earthquake(db.Model, SerializerMixin):
-    __tablename__ = "earthquakes"  # Name of the table in the database
+    __tablename__ = "earthquakes"  
 
     # Columns
-    id = Column(Integer, primary_key=True)  # Primary key
-    magnitude = Column(Float)  # To store earthquake magnitude
-    location = Column(String)  # To store the earthquake location
-    year = Column(Integer)  # To store the year of the earthquake
+    id = Column(Integer, primary_key=True)  
+    magnitude = Column(Float)  
+    location = Column(String)  
+    year = Column(Integer)  
 
     def __repr__(self):
         return f"<Earthquake(id={self.id}, magnitude={self.magnitude}, location={self.location}, year={self.year})>"
